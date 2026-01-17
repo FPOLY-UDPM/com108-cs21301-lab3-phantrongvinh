c21e460039eea73a5f0e9cdfee58dbba27e8ba58
phantrongvinh <1phantrongvinh98@gmail.com>
2026-01-17 08:46:49 +0700

    Nộp thử bài

--- bai1.c
+++ bai1.c
@@ -4,28 +4,58 @@
  * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
  *****************************************************************************/
 
-// BÀI 1: XÂY DỰNG CHƯƠNG TRÌNH TÍNH HỌC LỰC  
-// Input: Nhập vào điểm của sinh viên (0-10) 
-// Output: Hiển thị ra màn hình học lực của sinh viên 
-// Biết rằng: 
-// Học lực xuất sắc: Điểm >= 9 
-// Học lực giỏi: 9 > điểm >= 8 
-// Học lực khá: 8 > điểm >= 6.5 
-// Học lực trung bình: 6.5 > điểm >= 5 
-// Học lực yếu: 5 > điểm >= 3.5 
+// BÀI 1: XÂY DỰNG CHƯƠNG TRÌNH TÍNH HỌC LỰC
+// Input: Nhập vào điểm của sinh viên (0-10)
+// Output: Hiển thị ra màn hình học lực của sinh viên
+// Biết rằng:
+// Học lực xuất sắc: Điểm >= 9
+// Học lực giỏi: 9 > điểm >= 8
+// Học lực khá: 8 > điểm >= 6.5
+// Học lực trung bình: 6.5 > điểm >= 5
+// Học lực yếu: 5 > điểm >= 3.5
 // Học lực kém: 3.5 > điểm
 
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
     // Khai báo biến
-
+    float diem = 0.00;
 
     // Nhập dữ liệu
-
+    printf("Nhập điểm: ");
+    scanf("%f", &diem);
 
     // Xử lý, tính toán VÀ Hiển thị kết quả
+    if (diem <= 0)
+    {
+        printf("Diem khong hop le");
+    }
+    else if (diem >= 9 && diem <= 10)
+    {
+        printf("Hoc sinh xuat sac");
+    }
+    else if (diem >= 8 && diem < 9)
+    {
+        printf("Hoc sinh gioi");
+    }
+    else if (diem >= 6.5 && diem < 8)
+    {
+        printf("Hoc sinh kha");
+    }
+    else if (diem >= 5 && diem < 6.5)
+    {
+        printf("Hoc sinh trung binh");
+    }
+    else if (diem >= 3.5 && diem < 5)
+    {
+        printf("Hoc sinh yeu");
+    }
+    else
+    {
+        printf("Hoc sinh kem");
+    }
 
     return 0;
 }
--- bai2_1.c
+++ bai2_1.c
@@ -4,27 +4,26 @@
  * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
  *****************************************************************************/
 
-//  BÀI 2: XÂY DỰNG CHƯƠNG TRÌNH GIẢI PHƯƠNG TRÌNH  
-//  2.1 BẬC 1 
-//  Phương trình có dạng ax + b = 0 
-//  Input: nhập vào từ bàn phím giá trị của a, b 
-//  Output: Hiển thị nghiệm của phương trình 
-//  Biết rằng: 
-//  Nếu a == 0,  
-//   b == 0: Phương trình có vô số nghiệm 
-//   b != 0: Phương trình vô nghiệm 
-//  Ngược lại: Phương trình có nghiệm x = -b/a 
- 
+//  BÀI 2: XÂY DỰNG CHƯƠNG TRÌNH GIẢI PHƯƠNG TRÌNH
+//  2.1 BẬC 1
+//  Phương trình có dạng ax + b = 0
+//  Input: nhập vào từ bàn phím giá trị của a, b
+//  Output: Hiển thị nghiệm của phương trình
+//  Biết rằng:
+//  Nếu a == 0,
+//   b == 0: Phương trình có vô số nghiệm
+//   b != 0: Phương trình vô nghiệm
+//  Ngược lại: Phương trình có nghiệm x = -b/a
+
 #include <stdio.h>
 
-int main(){
-    
-    // Khai báo biến
+int main()
+{
 
+    // Khai báo biến
+    float a, b, x = 0;
 
     // Nhập dữ liệu
 
-
     // Xử lý, tính toán VÀ Hiển thị kết quả
-
 }
